93dbbc0e5ccedbaf5dfc9a0ea700f1cca7e9d78f
Matin Sasanpour <matin.sasan@gmail.com>
2022-04-29 09:00:16 +0430

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Minesweeper
+from tkinter import *
+
+# the most basic element; a regular window
+root = Tk()
+
+
+# with this it will wait until it's closed
+# All the code will be bewtween Tk() and mainloop()
+root.mainloop()
